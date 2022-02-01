28765ed863fbfd0bcd3cf73cd70d7458516cd45b
Denys Liienko <lienko.d.k@gmail.com>
2022-02-01 19:09:33 +0200

    add-enums-and-enities

---
+++ ReqPlanner.Backend/ReqPlanner.Domain/Entities/TimeScheduledCall.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Abstractions;
+using System;
+
+namespace Domain.Entities
+{
+    public class TimeScheduledCall : Call
+    {
+        DateTime _execTime;
+
+        public DateTime ExecTime
+        {
+            get { return _execTime; }
+            set { 
+                if (value <= DateTime.Now)
+                    throw new ArgumentOutOfRangeException("Execution time can't be in past");
+                _execTime = value;
+            }
+        }
+
+        public override bool IsConditionMet(object? condition) => IsConditionMet();
+
+        private bool IsConditionMet()
+        {
+            if (_execTime >= DateTime.Now)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ ReqPlanner.Backend/ReqPlanner.Domain/Enums/CallStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public enum CallStatus
+    {
+        Scheduled = 0,
+        Archived = 1,
+        Ok = 200,
+        Failed = 400
+    }
+}
