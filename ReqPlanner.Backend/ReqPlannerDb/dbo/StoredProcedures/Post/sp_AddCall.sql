fac1bd386221b4109e58a522682ef583f94d75bc
Denys Liienko <lienko.d.k@gmail.com>
2022-02-02 21:55:34 +0200

    add-post-stored-procedures

---
+++ ReqPlanner.Backend/ReqPlannerDb/dbo/StoredProcedures/Post/sp_AddCall.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[sp_AddCall]
+    @Title nvarchar(50), 
+    @Url nvarchar(100), 
+    @UserId int
+AS
+begin
+    declare @TempIdTable table(Id int)
+    declare @CallId int
+
+	insert into Calls ([Title], [Url], [UserId], [CreationUtcTime])
+        output inserted.Id into @TempIdTable
+    values (@Title, @Url, @UserId, GETUTCDATE())
+
+    set @CallId = (select [Id] from @TempIdTable)
+    return @CallId
+end
